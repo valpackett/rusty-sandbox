1c3b705d3478852fb9687b03f8d4f422817ce236
Greg V <greg@unrelenting.technology>
2018-06-10 14:54:32 +0300

    OpenBSD: allow proc/exec

--- src/platform/openbsd.rs
+++ src/platform/openbsd.rs
@@ -7,7 +7,7 @@ pub fn enter_sandbox<'a>(_: Box<Iterator<Item=&'a fs::Directory> + 'a>) -> bool 
     unsafe {
         // Path whitelist is going to be in 6.4: https://www.openbsd.org/papers/BeckPledgeUnveilBSDCan2018.pdf
         // 'error' makes sandbox violations return ENOSYS instead of exploding the program.
-        let promises = CString::new("stdio error").unwrap();
+        let promises = CString::new("stdio proc exec error").unwrap();
         // Second arg is execpromises, i.e. the pledges for exec()'d processes.
         // We don't want exec to quit the sandbox, of course.
         pledge(promises.as_ptr(), promises.as_ptr()) == 0
